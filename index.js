fc5dd778dadbe8ef3913a0e0dd7bdafa197569ed
Grapeia <rodrigo.grapeia@superare.co>
2021-02-25 18:29:31 -0300

    changed script

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  //.get('/', (req, res) => res.render('pages/test'))
   .get('/upload', (req, res) => {
     res.sendStatus(200);
   })
