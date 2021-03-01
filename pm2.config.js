c2f2ccd8227d4bd9fca6752c8071bfacb00ccea4
Grapeia <rodrigo.grapeia@superare.co>
2021-02-28 21:58:57 -0300

    fix

--- pm2.config.js
+++ pm2.config.js
@@ -2,15 +2,10 @@ module.exports = {
     apps: [{
         name: "velocirapid",
         script: "./index.js",
-        watch: false,
-        env: {
-            "NODE_ENV": "DEV",
-        },
-        env_production: {
-            "NODE_ENV": "PROD"
-        }
+        watch: false
     }, {
         name: "deploy-watcher",
-        script: "./deploy.js"
+        script: "./deploy.js",
+        watch: false
     }]
 }
