e83493342eaf101b379d109d25f3ee30222a8f5e
Grapeia <rodrigo.grapeia@superare.co>
2021-03-01 01:25:24 -0300

    fix

--- assets/main.js
+++ assets/main.js
@@ -176,7 +176,7 @@ function ulTest() {
     if (ulCalled) return;
     ulCalled = true;
 
-    ShowProgressMessage("upload", "Getting dino upload...");
+    ShowProgressMessage("upload", "Getting upload...");
 
     var startUpload, totalUploaded = 0;
 
@@ -262,7 +262,7 @@ function clearRequests(xhr) {
 }
 
 function InitiateSpeedDetection() {
-    ShowProgressMessage("download", "Getting dino speed...");
+    ShowProgressMessage("download", "Getting speed...");
     getPing();
     window.setTimeout(checkFiles, 2000);
     window.setTimeout(getIp, 1000);
