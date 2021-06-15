5cfe9f4fdb968002be210565b735c91ebc0943c6
Grapeia <rodrigo.grapeia@superare.co>
2021-06-15 15:26:31 -0300

    Changes to make this repo public

--- alert.js
+++ alert.js
@@ -1,24 +1,24 @@
 const request = require('request')
-const webhook = "https://chat.googleapis.com/v1/spaces/AAAAS3IsPkc/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=PAje_0SJE5HXnYMoGGhXpllcptwOAqtpu8AtKxvCEd0%3D";
+const webhook = process.env.ALERT_URL || false;
 
 var alertService = {
     sendAlert: (message) => {
-        request({
-            url: webhook,
-            method: 'POST',
-            json: {
-                'text': message,
-            }
-        }, (error, response, body) => {
-            if (error) {
-                console.log('Error sending message: ', error)
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error)
-            }
-        })
-
+        if (webhook) {
+            request({
+                url: webhook,
+                method: 'POST',
+                json: {
+                    'text': message,
+                }
+            }, (error, response, body) => {
+                if (error) {
+                    console.log('Error sending message: ', error)
+                } else if (response.body.error) {
+                    console.log('Error: ', response.body.error)
+                }
+            })
+        }
     }
 }
-
 
 module.exports = Object.create(alertService);
